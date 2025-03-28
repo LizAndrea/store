8dab561fb6710ac52651217b1fabcf9c8f97fb6f
LizAndrea <lizandrearamos17@gmail.com>
2025-03-27 23:38:16 -0400

    creacion de componentes, pipes y directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './domains/shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: ' <router-outlet/>'
+  //<app-header></app-header>
 })
 export class AppComponent {
   title = 'store';
---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, signal, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  hideSideMenu = signal(true);
+  private cartService = inject(CartService);
+
+  cart = this.cartService.cart;
+  total = this.cartService.total;
+
+  toogleSideMenu() {
+    this.hideSideMenu.update(prevState => !prevState);
+  }
+
+  removeProduct(product: Product) {
+    //this.cart = this.cart.filter(p => p !== product);
+    //this.total.set(this.calcTotal());
+  }
+}
